ac4bfc07759e3a5ad50928d91e82c5300d6d8115
deusart <dmitry.tavakalov@gmail.com>
2022-10-25 16:30:46 +0300

    [python] config template

---
+++ config/config.py
@@ -0,0 +1,15 @@
+# swgohhelp
+LOGIN = '---'
+PASSWORD = '---'
+ALLYCODE = 123
+
+# HOME SQL
+SERVER = '--\--'
+SQLUSERNAME = '--'
+SQLPASSWORD = '--'
+DATABASE = 'swgoh'
+
+# files
+LANDING = '.\stage'
+SOURCE = '.\source'
+LOG = '.'
